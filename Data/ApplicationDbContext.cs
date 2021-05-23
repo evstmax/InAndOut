72c8ad4dd037127c1851efb48b2e1d209c25a536
ems <ems82@ya.ru>
2021-05-23 10:34:11 +0300

    Добавьте файлы проекта.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using InAndOut.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InAndOut.Data
+{
+    public class ApplicationDbContext : DbContext
+
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
---
+++ Models/Expense.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InAndOut.Models
+{
+    public class Expense
+    {
+        [Key] 
+        public int Id { get; set; }
+        [DisplayName("Expense Name")]
+        [Required]
+        public string ExpenseName { get; set; }
+        [DisplayName("Amount")]
+        [Column(TypeName = "decimal(18, 2)")]
+        [Required]
+        [Range((double)decimal.MinValue, (double)decimal.MaxValue, ErrorMessage = "Please enter valid amount")]
+        public decimal ExpenseValue { get; set; }
+
+    }
+}
